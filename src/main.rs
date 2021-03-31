f70c0e94af3087d4d9547a32e0384a1e8e31bf56
Casper Verhaar <casper.verhaar@gmail.com>
2021-03-31 21:39:12 +0200

    Get input from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, stdin, stdout, Write};
+
+fn main() -> io::Result<()> {
+    print_prompt()?;
+
+    let mut input = String::new();
+
+    stdin().read_line(&mut input)?;
+
+    println!("Storing '{}'", input.trim());
+    Ok(())
+}
+
+fn print_prompt() -> io::Result<()> {
+    print!("> ");
+    stdout().flush()
 }
