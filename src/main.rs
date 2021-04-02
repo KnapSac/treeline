d5bbec36532ea5f2c35ef226aa88d9bf759fb5e4
Casper Verhaar <casper.verhaar@gmail.com>
2021-04-02 11:40:00 +0200

    Read input via crossterm
    To enable more control over the terminal, the crossterm crate was added.
    
    Because we want access the the `KeyEvent`s, we need to put the terminal
    into raw mode. This means that we also need to implement things like
    backspace and Ctrl+C ourselves.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,75 @@
-use std::io::{self, stdin, stdout, Write};
+use crossterm::{
+    cursor,
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
+    terminal::{self, ClearType},
+    QueueableCommand,
+};
+use std::io::{self, stdout, Write};
 use thiserror::Error;
 
-fn main() -> Result<()> {
+fn main() {
+    let result = run();
+
+    terminal::disable_raw_mode().ok();
+
+    match result {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run() -> Result<()> {
+    terminal::enable_raw_mode()?;
     print_prompt()?;
 
-    let mut input = String::new();
+    let mut line_buffer = String::new();
+    while let Event::Key(event) = read()? {
+        let mut input = None;
+        match event {
+            KeyEvent {
+                modifiers: KeyModifiers::CONTROL,
+                code: KeyCode::Char('c'),
+            } => {
+                return Ok(());
+            }
+            KeyEvent {
+                code: KeyCode::Enter,
+                ..
+            } => {
+                break;
+            }
+            KeyEvent {
+                code: KeyCode::Backspace,
+                ..
+            } => {
+                line_buffer.pop();
+                stdout()
+                    .queue(cursor::MoveLeft(1))?
+                    .queue(terminal::Clear(ClearType::UntilNewLine))?;
+                stdout().flush()?;
+            }
+            KeyEvent {
+                code: KeyCode::Char(c),
+                ..
+            } => {
+                line_buffer.push(c);
+                input = Some(c);
+            }
+            _ => {}
+        }
 
-    stdin().read_line(&mut input)?;
+        if let Some(c) = input {
+            print!("{}", c);
+            stdout().flush()?;
+        }
+    }
 
-    println!("Storing '{}'", input.trim());
+    println!();
+    println!("{}", line_buffer);
+
     Ok(())
 }
 
@@ -25,4 +86,7 @@ type Result<R, E = Error> = std::result::Result<R, E>;
 pub enum Error {
     #[error(transparent)]
     IoError(#[from] io::Error),
+
+    #[error(transparent)]
+    CrosstermError(#[from] crossterm::ErrorKind),
 }
