1034e044efff935f2b251fcf03b25e4189f506a0
Casper Verhaar <casper.verhaar@gmail.com>
2021-04-10 20:53:29 +0200

    Return reference to last node from `find`
    Instead of just returning whether the trie contains a given input, we
    can provide a more flexible API by returning a reference to the `Node`
    containing the last character of the input. This enables users to search
    for a prefix, and continue searching from that prefix, instead of having
    to search for the entire input each time.

--- src/trie.rs
+++ src/trie.rs
@@ -30,9 +30,8 @@ impl Trie {
         self.root.insert(input);
     }
 
-    // TODO CV: Is a `bool` the most appropriate thing to return here?
-    /// Returns true if the trie contains the input.
-    pub fn find(&self, input: &str) -> bool {
+    /// Returns a reference to the [`Node`] containing the last character of the input.
+    pub fn find(&self, input: &str) -> Option<&Node> {
         self.root.find(input)
     }
 }
@@ -64,17 +63,17 @@ impl Node {
         }
     }
 
-    /// Returns true if the input is contained in the descendants of the current node.
-    pub fn find(&self, input: &str) -> bool {
+    /// Returns a reference to the [`Node`] containing the last character of the input.
+    pub fn find(&self, input: &str) -> Option<&Self> {
         if let Some(root) = input.chars().nth(0) {
             if let Some(child) = self.children.get(&root) {
                 return child.find(&input[1..]);
             } else {
-                return false;
+                return None;
             }
         }
 
-        true
+        Some(self)
     }
 }
 
@@ -88,8 +87,8 @@ mod tests {
         let input = "Hello world!";
 
         trie.insert(input);
-        assert!(trie.find(input));
-        assert!(!trie.find("Hi there"));
+        assert!(trie.find(input).is_some());
+        assert!(trie.find("Hi there").is_none());
     }
 
     #[test]
@@ -103,17 +102,17 @@ mod tests {
         trie.insert(input2);
         trie.insert(input3);
 
-        assert!(trie.find(input1));
-        assert!(trie.find(input2));
-        assert!(trie.find(input3));
-        assert!(!trie.find("Hi there"));
+        assert!(trie.find(input1).is_some());
+        assert!(trie.find(input2).is_some());
+        assert!(trie.find(input3).is_some());
+        assert!(trie.find("Hi there").is_none());
     }
 
     #[test]
     fn find_in_empty_trie() {
         let trie = Trie::new();
 
-        assert!(!trie.find(" "));
+        assert!(trie.find(" ").is_none());
     }
 
     #[test]
@@ -121,6 +120,6 @@ mod tests {
         let mut trie = Trie::new();
         trie.insert("Hello world!");
 
-        assert!(trie.find("Hello"));
+        assert!(trie.find("Hello").is_some());
     }
 }
